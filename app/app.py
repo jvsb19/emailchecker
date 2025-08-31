97729b112a423825ff6584e44f3a7d1254cdc307
Joao Vitor <jvsb19@gmail.com>
2025-08-31 15:00:38 -0300

    fix Flask app para heroku

--- app/app.py
+++ app/app.py
@@ -1,11 +1,8 @@
 from flask import Flask, request, jsonify
 from func import routes
 
-def run_app():
-    app = Flask(__name__)
-    routes(app)
-    return app
+app = Flask(__name__)
+routes(app)
 
 if __name__ == "__main__":
-    app = run_app()
     app.run(debug=True)
