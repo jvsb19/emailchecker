96c1fc01168e1d8e4e0afc12ac0142daba35fc41
Joao Vitor <jvsb19@gmail.com>
2025-08-31 18:19:11 -0300

    att classifier

--- app/services/classifier.py
+++ app/services/classifier.py
@@ -1,6 +1,3 @@
-<<<<<<< HEAD
-<<<<<<< HEAD
-<<<<<<< HEAD
 from transformers import pipeline 
 import torch
 
@@ -37,51 +34,6 @@ def email_response(category):
     elif category == "Improdutivo": 
         return "Esse e-mail não requer uma ação ou resposta imediata." 
     return "Indefinido"
-=======
-from dotenv import load_dotenv
-=======
->>>>>>> 7a262bb (atualizacao classifier e gitignore)
-from transformers import pipeline
-=======
-from transformers import pipeline 
->>>>>>> 4d47a84 (deploy heroku)
-import torch
 
-classifier = pipeline( 
-    "zero-shot-classification", 
-    model="facebook/bart-large-mnli", 
-    device=0 if torch.cuda.is_available() else -1 
-) 
 
-produtivo_labels = {
-    "work", "help", "urgency", "refund", "technical support", "system acess", "system problem", "work budget", "error"
-}
 
-improdutivo_labels = {
-    "non-work","congratulations", "joke", "automatic", "invitation", "personal", "greet", "vacation", "break", "checklist", "spam", "thank", "opinion"
-}
-
-def email_classify(text): 
-    text_lower = text.lower() 
-
-    resultado = classifier( 
-        text_lower, 
-        candidate_labels=list(produtivo_labels | improdutivo_labels), 
-        hypothesis_template="This email is about {}."
-    )
-
-    if resultado["labels"][0] in produtivo_labels:
-        return "Produtivo"
-    return "Improdutivo"
-<<<<<<< HEAD
-            
->>>>>>> 63b72b2 (classifier funcional)
-=======
-
-def email_response(category): 
-    if category == "Produtivo": 
-        return "Esse e-mail requer uma ação ou resposta imediata." 
-    elif category == "Improdutivo": 
-        return "Esse e-mail não requer uma ação ou resposta imediata." 
-    return "Indefinido"
->>>>>>> 4d47a84 (deploy heroku)
